0feb3c1befabc09ce0c3f22319db67e87036707e
Gombos Alex <alex.gombos01@e-uvt.ro>
2023-05-29 09:31:14 +0300

    fixed length limit problem and added pdf upload

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import torch
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 import json
 from datasets import Dataset, ClassLabel, Sequence
+import io
+from PyPDF2 import PdfReader
 
 app = Flask(__name__)
 
@@ -20,6 +22,25 @@ dataset = dataset.cast_column("ner_tags", Sequence(ClassLabel(names=label_names)
 
 model = AutoModelForTokenClassification.from_pretrained(r"D:\Alex\Licenta\model")
 
+def split_string(string, max_length):
+    words = string.split()
+    split_strings = []
+    current_string = ""
+    word_count = 0
+
+    for word in words:
+        if word_count + len(word.split()) <= max_length:
+            current_string += word + " "
+            word_count += len(word.split())
+        else:
+            split_strings.append(current_string.strip())
+            current_string = word + " "
+            word_count = len(word.split())
+
+    if current_string:
+        split_strings.append(current_string.strip())
+
+    return split_strings
 
 # define a function to predict labels for an input sentence
 def predict_labels(input_sentence):
@@ -90,21 +111,72 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # get the input sentence from the form
-    input_sentence = request.form['sentence']
 
-    # predict the labels for the input sentence
-    predicted_labels = predict_labels(input_sentence)
+    # Check if a file was uploaded
+    if 'file' in request.files:
+        file = request.files['file']
+        # Check if the file has a PDF extension
+        if file.filename.endswith('.pdf'):
+            # Read the uploaded PDF file
+            pdf_reader = PdfReader(file)
+            text = ""
+            for page in range(len(pdf_reader.pages)):
+                text += pdf_reader.pages[page].extract_text()
 
-    decoded_input = decode_input(input_sentence)
+        max_length = 250
+        result = split_string(text, max_length)
 
-    word_list = print_every(decoded_input, predicted_labels)
+        predicted_labels_all = []
+        decoded_input_all = []
 
-    print(word_list)
-    print(predicted_labels)
-    print(decoded_input)
-    # display the predicted labels on the webpage
-    return render_template('index.html', predicted_labels=predicted_labels, decoded_input = decoded_input, tokenizer=tokenizer, word_list=word_list)
+        for i, s in enumerate(result):
+
+            # predict the labels for the input sentence
+            predicted_labels = predict_labels(s)
+
+            decoded_input = decode_input(s)
+
+            # word_list = print_every(decoded_input, predicted_labels)
+
+            predicted_labels_all.extend(predicted_labels)
+            decoded_input_all.extend(decoded_input)
+
+            word_list = print_every(decoded_input_all, predicted_labels_all)
+
+        return render_template('index.html', predicted_labels=predicted_labels, decoded_input = decoded_input, tokenizer=tokenizer, word_list=word_list)
+
+    else:
+
+        # get the input sentence from the form
+        input_sentence = request.form['sentence']
+
+        max_length = 250
+        result = split_string(input_sentence, max_length)
+
+        predicted_labels_all = []
+        decoded_input_all = []
+
+        for i, s in enumerate(result):
+
+            # predict the labels for the input sentence
+            predicted_labels = predict_labels(s)
+
+            decoded_input = decode_input(s)
+
+            # word_list = print_every(decoded_input, predicted_labels)
+
+            predicted_labels_all.extend(predicted_labels)
+            decoded_input_all.extend(decoded_input)
+
+
+            # print(word_list)
+            # print(predicted_labels)
+            # print(decoded_input)
+        print(decoded_input_all)
+        print(predicted_labels_all)
+        word_list = print_every(decoded_input_all, predicted_labels_all)
+        # display the predicted labels on the webpage
+        return render_template('index.html', predicted_labels=predicted_labels, decoded_input = decoded_input, tokenizer=tokenizer, word_list=word_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
