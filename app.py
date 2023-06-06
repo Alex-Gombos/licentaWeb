9e830176bb40a039fe2f3cb09617dbfac7157d3e
Gombos Alex <alex.gombos01@e-uvt.ro>
2023-06-06 21:55:06 +0300

    commit

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import torch
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 import json
 from datasets import Dataset, ClassLabel, Sequence
+import io
+from PyPDF2 import PdfReader
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')  # Add the static_folder parameter here
 
 # load the tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained("dumitrescustefan/bert-base-romanian-uncased-v1")
@@ -20,6 +22,25 @@ dataset = dataset.cast_column("ner_tags", Sequence(ClassLabel(names=label_names)
 
 model = AutoModelForTokenClassification.from_pretrained(r"D:\Alex\Licenta\model")
 
+def split_string(string, max_length):
+    words = string.split()
+    split_strings = []
+    current_string = ""
+    word_count = 0
+
+    for word in words:
+        if word_count + len(word.split()) <= max_length:
+            current_string += word + " "
+            word_count += len(word.split())
+        else:
+            split_strings.append(current_string.strip())
+            current_string = word + " "
+            word_count = len(word.split())
+
+    if current_string:
+        split_strings.append(current_string.strip())
+
+    return split_strings
 
 # define a function to predict labels for an input sentence
 def predict_labels(input_sentence):
@@ -42,8 +63,24 @@ def decode_input(input_sentence):
 
 def concat_new(input):
     concatenated_text = []
-    for line in input:
+    buffer = ""
+    for index,line in enumerate(input):
         newLine = ""
+        if line[0] == '#':
+            words = concatenated_text[-1].split()
+            last_word = words[-1]
+            temp = ' '.join(concatenated_text[-1].split(' ')[:-1])
+            concatenated_text[-1] = temp
+            newLine = newLine + last_word + " "
+        if "[CLS]" in line:
+            temp = line.replace("[CLS]", "")
+            line = temp
+        if "[SEP]" in line:
+            temp = line.replace("[SEP]", "")
+            line = temp
+        if "[UNK]" in line:
+            temp = line.replace("[UNK]", "")
+            line = temp
         if "##" in line:
             i = 0
             for charater in line:
@@ -57,7 +94,8 @@ def concat_new(input):
             concatenated_text.append(newLine)
         else:
             concatenated_text.append(line)
-    print(concatenated_text)
+        last_word_index = index
+    #print(concatenated_text)
     return concatenated_text
 
 def print_every(decoded_input , predicted_labels):
@@ -69,7 +107,7 @@ def print_every(decoded_input , predicted_labels):
         # if the label has changed, print the current word and its label
         if label != current_label:
             if current_word:
-                print(current_word, current_label)
+                #print(current_word, current_label)
                 list1.append(current_word)
                 list2.append(current_label)
             current_word = token
@@ -90,21 +128,62 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # get the input sentence from the form
-    input_sentence = request.form['sentence']
 
-    # predict the labels for the input sentence
-    predicted_labels = predict_labels(input_sentence)
+    # Check if a file was uploaded
+    if 'submit_file' in request.form:
+        print("FILE")
+        file = request.files['file']
+        # Check if the file has a PDF extension
+        if file.filename.endswith('.pdf'):
+            # Read the uploaded PDF file
+            pdf_reader = PdfReader(file)
+            text = ""
+            for page in range(len(pdf_reader.pages)):
+                text += pdf_reader.pages[page].extract_text()
 
-    decoded_input = decode_input(input_sentence)
+        max_length = 250
+        result = split_string(text, max_length)
 
-    word_list = print_every(decoded_input, predicted_labels)
+        predicted_labels_all = []
+        decoded_input_all = []
 
-    print(word_list)
-    print(predicted_labels)
-    print(decoded_input)
-    # display the predicted labels on the webpage
-    return render_template('index.html', predicted_labels=predicted_labels, decoded_input = decoded_input, tokenizer=tokenizer, word_list=word_list)
+        for i, s in enumerate(result):
+
+            # predict the labels for the input sentence
+            predicted_labels = predict_labels(s)
+
+            decoded_input = decode_input(s)
+
+            predicted_labels_all.extend(predicted_labels)
+            decoded_input_all.extend(decoded_input)
+
+            word_list = print_every(decoded_input_all, predicted_labels_all)
+
+        return render_template('index.html', predicted_labels=predicted_labels, decoded_input = decoded_input, tokenizer=tokenizer, word_list=word_list)
+
+    elif 'submit_text' in request.form:
+        print("TEXT")
+        # get the input sentence from the form
+        input_sentence = request.form['sentence']
+
+        max_length = 250
+        result = split_string(input_sentence, max_length)
+
+        predicted_labels_all = []
+        decoded_input_all = []
+
+        for i, s in enumerate(result):
+
+            predicted_labels = predict_labels(s)
+
+            decoded_input = decode_input(s)
+
+            predicted_labels_all.extend(predicted_labels)
+            decoded_input_all.extend(decoded_input)
+        
+        word_list = print_every(decoded_input_all, predicted_labels_all)
+        # display the predicted labels on the webpage
+        return render_template('index.html', predicted_labels=predicted_labels, decoded_input = decoded_input, tokenizer=tokenizer, word_list=word_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
